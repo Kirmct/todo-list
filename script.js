5834bb2e3e23cfaa434bb64a9d4c8a60bcf277fe
Kirmct Abreu <kirmctneto@gmail.com>
2023-02-14 17:42:50 -0300

    salvo no local storage

--- script.js
+++ script.js
@@ -4,6 +4,65 @@ let lista = [];
 let dataAtual;
 let taskAtual;
 
+const load = () => {
+  if (lista.length > 0) {
+    lista.forEach((li) => {
+      const texto = li.text;
+      const ul = document.querySelector(`.item-list`);
+
+      const newItem = document.createElement(`li`);
+      const newDiv = document.createElement(`div`);
+      const newCheck = document.createElement(`input`);
+      const newSpan = document.createElement(`span`);
+
+      const newDivButton = document.createElement(`div`);
+      const removeButton = document.createElement(`a`);
+      const newEdtButton = document.createElement(`a`);
+
+      newDivButton.classList.add(`item--btns`);
+      removeButton.setAttribute(`href`, "#");
+      removeButton.classList.add(`btn`);
+      removeButton.innerHTML = `⛔`;
+      removeButton.addEventListener(`click`, (item) => {
+        removerItem(item);
+      });
+
+      newEdtButton.setAttribute(`href`, "#");
+      newEdtButton.classList.add(`btn`);
+      newEdtButton.innerHTML = `✏️`;
+      newEdtButton.addEventListener(`click`, (item) => {
+        modificar(item);
+      });
+
+      newDivButton.append(newEdtButton);
+      newDivButton.append(removeButton);
+
+      newDiv.classList.add(`item--content`);
+      newCheck.setAttribute(`type`, `checkbox`);
+      newDiv.append(newCheck);
+      newSpan.innerText = texto;
+      newDiv.append(newSpan);
+
+      newItem.setAttribute(`data-key`, li.id);
+      newItem.append(newDiv);
+      newItem.append(newDivButton);
+      newItem.classList.add(`item`);
+      ul.append(newItem);
+      input.value = ``;
+      data++;
+    });
+  }
+};
+
+if (localStorage.hasOwnProperty("data")) {
+  data = JSON.parse(localStorage.getItem(`data`)) || {};
+}
+
+if (localStorage.hasOwnProperty("lista")) {
+  lista = JSON.parse(localStorage.getItem(`lista`)) || {};
+  load();
+}
+
 const adicionarItem = (e) => {
   if (e.key == `Enter`) {
     const texto = input.value;
@@ -24,7 +83,7 @@ const adicionarItem = (e) => {
       removeButton.classList.add(`btn`);
       removeButton.innerHTML = `⛔`;
       removeButton.addEventListener(`click`, (item) => {
-        alertar(item);
+        removerItem(item);
       });
 
       newEdtButton.setAttribute(`href`, "#");
@@ -49,16 +108,30 @@ const adicionarItem = (e) => {
       newItem.classList.add(`item`);
       ul.append(newItem);
       input.value = ``;
-      data++;
       lista.push({ id: data, text: texto });
+      save();
+      data++;
     }
   }
 };
 
 //funcao de remover
-function alertar(el) {
-  const atualLi = el.currentTarget.closest(`li`);
-  atualLi.remove();
+function removerItem(el) {
+  const parentElement = el.currentTarget.closest(`li`);
+  let key = parentElement.getAttribute(`data-key`);
+  console.log(`data key ${key}`);
+  let index = 0;
+  lista.forEach((list, i) => {
+    if (list.id == key) {
+      console.log(`indice ${i}`);
+      console.log(list.text);
+
+      index = i;
+    }
+  });
+  lista.splice(index, 1);
+  parentElement.remove();
+  save();
 }
 
 function modificar(el) {
@@ -68,7 +141,6 @@ function modificar(el) {
 
   let parentElement = el.currentTarget.closest(`li`);
   taskAtual = parentElement.querySelector(`span`).innerText;
-  console.log(taskAtual);
   modal.querySelector(`span`).innerHTML = taskAtual;
   if (parentElement && parentElement.getAttribute(`data-key`)) {
     key = parentElement.getAttribute(`data-key`);
@@ -102,6 +174,14 @@ const updateTodo = (text) => {
       todo.querySelector(`span`).innerHTML = text;
     }
   });
+  save();
 };
 
 input.addEventListener(`keyup`, adicionarItem);
+
+//salvar no local storage
+function save() {
+  let newList = Object.assign(lista);
+  localStorage.setItem(`lista`, JSON.stringify(newList));
+  localStorage.setItem(`data`, JSON.stringify(data));
+}
